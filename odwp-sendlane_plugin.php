599914782988122e99afb03b4de8e63c5b1d102e
Ondřej Doněk <ondrejd@gmail.com>
2017-08-26 23:21:18 +0200

    Finishing the plugin.

--- odwp-sendlane_plugin.php
+++ odwp-sendlane_plugin.php
@@ -2,13 +2,15 @@
 /**
  * Plugin Name: Sendlane plugin
  * Plugin URI: https://github.com/ondrejd/odwp-sendlane_plugin
- * Description: Plugin pro integraci přihlášení uživatelů k hromadným emailům obsluhovaným pomocí <a href="https://sendlane.com/">Sendlane API</a>.
+ * Description: Plugin pro integraci přihlášení uživatelů k hromadným emailům obsluhovaným pomocí <a href="https://sendlane.com/" target="_blank">Sendlane API</a>.
  * Version: 1.0.0
  * Author: Ondřej Doněk
- * Author URI:
+ * Author URI: https://ondrejd.com/
  * License: GPLv3
  * Requires at least: 4.7
- * Tested up to: 4.7.4
+ * Tested up to: 4.8.1
+ * Tags: sendlane,mailing,users
+ * Donate link: https://www.paypal.me/ondrejd
  *
  * Text Domain: odwp-sendlane_plugin
  * Domain Path: /languages/
@@ -17,13 +19,14 @@
  * @link https://github.com/ondrejd/odwp-sendlane_plugin for the canonical source repository
  * @license https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License 3.0
  * @package odwp-sendlane_plugin
+ * @since 1.0.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-
+// Some constants
 include( dirname( __FILE__ ) . '/src/Sendlane_Api.php' );
 include( dirname( __FILE__ ) . '/src/Actions_List.php' );
 include( dirname( __FILE__ ) . '/src/Actions_Table.php' );
@@ -378,43 +381,9 @@ class odwpSendlanePlugin {
      * @return void
      */
     public static function render_admin_page_list() {
-?>
-<div class="wrap">
-    <h1><?php _e( 'Sendlane plugin', self::SLUG ) ?></h1>
-    <p class="description"><?php _e( 'Zde můžete nastavit cílové stránky a akce k nim připojené.', self::SLUG ) ?></p>
-    <form id="odwpsp-actions_table_form" method="get">
-        <table class="wp-list-table widefat fixed striped odwpsp-actions">
-            <thead>
-                <tr>
-                    <td id="cb" class="manage-column column-cb check-column">
-                        <label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Označit vše', self::SLUG ) ?></label>
-                        <input id="cb-select-all-1" type="checkbox">
-                    </td>
-                    <th scope="col" id="page" class="manage-column column-page column-primary"><?php _e( 'Cílová stránka', self::SLUG ) ?></th>
-                    <th scope="col" id="action" class="manage-column column-action"><?php _e( 'Akce', self::SLUG ) ?></th>
-                    <th scope="col" id="list_tag" class="manage-column column-list_tag"><?php _e( 'Seznam/tag', self::SLUG ) ?></th>
-                </tr>
-            </thead>
-            <tbody id="the-list">
-                <tr>
-                    <td colspan="4"><?php _e( 'Zatím nejsou vytvořeny žádné akce.', self::SLUG ) ?></td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td class="manage-column column-cb check-column">
-                        <label class="screen-reader-text" for="cb-select-all-1"><?php _e( 'Označit vše', self::SLUG ) ?></label>
-                        <input id="cb-select-all-2" type="checkbox">
-                    </td>
-                    <th scope="col" id="page" class="manage-column column-page column-primary"><?php _e( 'Cílová stránka', self::SLUG ) ?></th>
-                    <th scope="col" id="action" class="manage-column column-action"><?php _e( 'Akce', self::SLUG ) ?></th>
-                    <th scope="col" id="list_tag" class="manage-column column-list_tag"><?php _e( 'Seznam/tag', self::SLUG ) ?></th>
-                </tr>
-            </tfoot>
-        </table>
-    </form>
-</div>
-<?php
+        ob_start( function() {} );
+        include( dirname( __FILE__ ) . '/partials/setting-page_list.phtml' );
+        echo ob_get_flush();
     }
 
     /**
@@ -423,102 +392,9 @@ class odwpSendlanePlugin {
      * @todo Add `wpnonce` for the security!
      */
     public static function render_admin_page_add() {
-        $avail_pages = get_pages( [
-            'sort_order' => 'asc',
-            'hierarchical' => 0,
-            'child_of' => 0,
-            'post_type' => 'page',
-        ] );
-        $avail_lists = self::get_lists();
-        $avail_tags = self::get_tags();
-        $api_error = array_key_exists( 'error', $avail_lists ) || array_key_exists( 'error', $avail_tags );
-?>
-<div class="wrap">
-    <h1><?php _e( 'Přidat akci', self::SLUG ) ?></h1>
-    <?php if( array_key_exists( 'error', $avail_lists ) ) : 
-    foreach( $avail_lists['error'] as $msg) : 
-        self::print_error( __( '<b>Sendlane API error:</b>&nbsp;', self::SLUG ) . $msg, 'error' );
-    endforeach; 
-    endif; ?>
-    <?php if( array_key_exists( 'error', $avail_tags ) ) : 
-    foreach( $avail_tags['error'] as $msg) : 
-        self::print_error( __( '<b>Sendlane API error:</b>&nbsp;', self::SLUG ) . $msg, 'error' );
-    endforeach; 
-    endif; ?>
-    <form method="post" action="<?php echo admin_url( '?page=odwpsp_menu_add' ) ?>" novalidate="novalidate">
-        <input type="hidden" name="_wpnonce" value="">
-        <table class="form-table">
-            <tbody>
-                <tr>
-                    <th scope="row">
-                        <label for="odwpsp-page"><?php _e( 'Cílová stránka', self::SLUG ) ?></label>
-                    </th>
-                    <td>
-                        <select id="odwpsp-page" name="page">
-                            <?php foreach( $avail_pages as $page ) : ?>
-                            <option value="<?php echo $page->ID ?>"><?php echo $page->post_title ?></option>
-                            <?php endforeach ?>
-                        </select>
-                        <p class="description"><?php _e( 'Vyberte cílovou stránku.', self::SLUG ) ?></p>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">
-                        <label for="odwpsp-action"><?php _e( 'Akce', self::SLUG ) ?></label>
-                    </th>
-                    <td>
-                        <select id="odwpsp-action" name="action">
-                            <option value="subscribe"><?php _e( 'Přihlásit', self::SLUG ) ?></option>
-                            <option value="unsubscribe"><?php _e( 'Odhlásit', self::SLUG ) ?></option>
-                            <option value="tag_add"><?php _e( 'Přidat tag', self::SLUG ) ?></option>
-                            <option value="tag_remove"><?php _e( 'Odebrat tag', self::SLUG ) ?></option>
-                        </select>
-                        <p class="description"><?php _e( 'Akce k provedení přes Sendlane API', self::SLUG ) ?></p>
-                    </td>
-                </tr>
-                <tr id="odwpsp-lists_row">
-                    <th scope="row">
-                        <label for="odwpsp-lists"><?php _e( 'Seznam', self::SLUG ) ?></label>
-                    </th>
-                    <td>
-                        <?php if( $api_error === true ) : ?>
-                        <select id="odwpsp-lists" name="lists" disabled="disabled"></select>
-                        <p class="description" style="color: #f30;"><?php _e( 'Nastala chyba při získávání dat prostřednictvím <b>Sendlane API</b>!', self::SLUG ) ?></p>
-                        <?php else : ?>
-                        <select id="odwpsp-lists" name="lists">
-                            <?php foreach( $avail_lists as $list ) : ?>
-                            <option value="<?php echo $list['list_id'] ?>"><?php echo $list['list_name'] ?></option>
-                            <?php endforeach ?>
-                        </select>
-                        <?php endif ?>
-                    </td>
-                </tr>
-                <tr id="odwpsp-tags_row">
-                    <th scope="row">
-                        <label for="odwpsp-tags"><?php _e( 'Tag', self::SLUG ) ?></label>
-                    </th>
-                    <td>
-                        <?php if( $api_error === true ) : ?>
-                        <select id="odwpsp-tags" name="tags" disabled="disabled"></select>
-                        <p class="description" style="color: #f30;"><?php _e( 'Nastala chyba při získávání dat prostřednictvím <b>Sendlane API</b>!', self::SLUG ) ?></p>
-                        <?php else : ?>
-                        <select id="odwpsp-tags" name="tags">
-                            <?php foreach( $avail_tags as $tag ) : ?>
-                            <option value="<?php echo $tag['tag_id'] ?>"><?php echo $tag['tag_name'] ?></option>
-                            <?php endforeach ?>
-                        </select>
-                        <?php endif ?>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        <p class="submit">
-            <input type="submit" name="submit" id="submit" class="button button-primary" value="Přidat akci">&nbsp;
-            <input type="reset" name="reset" id="reset" class="button button-cancel" value="Zrušit">
-        </p>
-    </form>
-</div>
-<?php
+        ob_start( function() {} );
+        include( dirname( __FILE__ ) . '/partials/setting-page_add.phtml' );
+        echo ob_get_flush();
     }
 
     /**
@@ -531,27 +407,6 @@ class odwpSendlanePlugin {
         }
 
         // Nothing to do...
-    }
-
-    /**
-     * @internal Check requirements of the plugin.
-     * @link https://developer.wordpress.org/reference/functions/is_plugin_active_for_network/#source-code
-     * @return boolean Returns TRUE if requirements are met.
-     * @todo Current solution doesn't work for WPMU...
-     */
-    public static function requirements_check() {
-        //$active_plugins = (array) get_option( 'active_plugins', [] );
-        //return in_array( 'woocommerce/woocommerce.php', $active_plugins ) ? true : false;
-        return true;
-    }
-
-    /**
-     * @internal Shows error in WP administration that minimum requirements were not met.
-     * @return void
-     */
-    public static function requirements_error() {
-        //self::print_error( __( 'Plugin <b>Úpravy pro Estets.cz</b> vyžaduje, aby byl nejprve nainstalovaný a aktivovaný plugin <b>WooCommerce</b>.', 'odwpwcgp' ), 'error' );
-        //self::print_error( __( 'Plugin <b>Úpravy pro Estets.cz</b> byl <b>deaktivován</b>.', 'odwpwcgp' ), 'updated' );
     }
 
     /**
@@ -569,13 +424,5 @@ class odwpSendlanePlugin {
 
 endif;
 
-// Check requirements before plugin is loaded
-if( !odwpSendlanePlugin::requirements_check() ) {
-    odwpSendlanePlugin::deactivate_raw();
-
-    if( is_admin() ) {
-        add_action( 'admin_head', [odwpSendlanePlugin, 'requirements_error '] );
-    }
-} else {
-    odwpSendlanePlugin::init();
-}
+// Initialize plugin
+odwpSendlanePlugin::init();
